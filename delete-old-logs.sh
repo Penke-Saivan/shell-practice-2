c5811574ab9842b3b6bdca3d787cddd9b21d453e
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-29 13:41:42 +0530

    while

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -12,3 +12,11 @@ mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 SOURCE_DIR=/home/ec2-user/app-logs
+
+if [ ! -d $SOURCE_DIR ]; then
+     echo -e "ERROR:: $SOURCE_DIR does not exist"
+     exit 1
+fi     
+
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
---
+++ while.sh
@@ -0,0 +1,8 @@
+while [ $count -gt 0 ]
+do
+  echo "Time left: $count"
+  sleep 1 # Pause for 1 second
+  count=$((count - 1)) # Decrement the count
+done
+
+echo "Times up!"
